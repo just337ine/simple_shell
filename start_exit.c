ac55308a9c68412c6bee77a36dc426f1be3d0bb1
Ugwu Justine <ugwujutineobinna@gmail.com>
2023-07-19 01:19:34 +0100

    add start and exit function file

---
+++ start_exit.c
@@ -0,0 +1,53 @@
+#include "shs.h"
+
+/**
+ * exec_line - finds built-ins and commands
+ *
+ * @shdata: data relevant (args)
+ * Return: 1 on success.
+ */
+int exec_line(shell_data *shdata)
+{
+	int (*builtin)(shell_data *shdata);
+
+	if (shdata->args[0] == NULL)
+		return (1);
+
+	builtin = get_builtin(shdata->args[0]);
+
+	if (builtin != NULL)
+		return (builtin(shdata));
+
+	return (cmd_exec(shdata));
+}
+
+/**
+ * exit_shell - exits the shell
+ *
+ * @shdata: data relevant (status and args)
+ * Return: 0 on success.
+ */
+int exit_shell(shell_data *shdata)
+{
+	unsigned int ustatus;
+	int is_digit;
+	int str_len;
+	int big_number;
+
+	if (shdata->args[1] != NULL)
+	{
+		ustatus = _atoi(shdata->args[1]);
+		is_digit = _isdigit(shdata->args[1]);
+		str_len = _strlen(shdata->args[1]);
+		big_number = ustatus > (unsigned int)INT_MAX;
+		if (!is_digit || str_len > 10 || big_number)
+		{
+			get_error(shdata, 2);
+			shdata->status = 2;
+			return (1);
+		}
+		shdata->status = (ustatus % 256);
+	}
+
+	return (0);
+}
