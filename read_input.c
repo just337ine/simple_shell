f8f790bb3549e975b69c82769e7640c966725117
Justine Ugwu <ugwujustineobinna@gmail.com>
2023-07-19 02:53:55 +0100

    add input function

---
+++ read_input.c
@@ -0,0 +1,98 @@
+#include "shs.h"
+
+/**
+ * assign_line - assigns the line var for get_line
+ * @lineptr: Buffer that stores the input string
+ * @n: Size of lineptr
+ * @buffer: String that is been called to line
+ * @j: Size of buffer
+ */
+void assign_line(char **lineptr, size_t *n, char *buffer, size_t j)
+{
+	if (*lineptr == NULL)
+	{
+		if (j > BUFSIZE)
+			*n = j;
+		else
+			*n = BUFSIZE;
+		*lineptr = buffer;
+	}
+	else if (*n < j)
+	{
+		if (j > BUFSIZE)
+			*n = j;
+		else
+			*n = BUFSIZE;
+		*lineptr = buffer;
+	}
+	else
+	{
+		_strcpy(*lineptr, buffer);
+		free(buffer);
+	}
+}
+
+/**
+ * get_line - Read input from stream
+ * @lineptr: Buffer that stores the input
+ * @n: Size of lineptr
+ * @stream: Stream to read from
+ * Return: The number of bytes read
+ */
+ssize_t get_line(char **lineptr, size_t *n, FILE *stream)
+{
+	int i;
+	static ssize_t input;
+	ssize_t retval;
+	char *buffer;
+	char t = 'z';
+
+	if (input == 0)
+		fflush(stream);
+	else
+		return (-1);
+	input = 0;
+
+	buffer = malloc(sizeof(char) * BUFSIZE);
+	if (buffer == NULL)
+		return (-1);
+	while (t != '\n')
+	{
+		i = read(STDIN_FILENO, &t, 1);
+		if (i == -1 || (i == 0 && input == 0))
+		{
+			free(buffer);
+			return (-1);
+		}
+		if (i == 0 && input != 0)
+		{
+			input++;
+			break;
+		}
+		if (input >= BUFSIZE)
+			buffer = _realloc(buffer, input, input + 1);
+		buffer[input] = t;
+		input++;
+	}
+	buffer[input] = '\0';
+	assign_line(lineptr, n, buffer, input);
+	retval = input;
+	if (i != 0)
+		input = 0;
+	return (retval);
+}
+/**
+ * _get_line - reads the input string.
+ *
+ * @i_eof: return value of getline function
+ * Return: input string
+ */
+char *_get_line(int *i_eof)
+{
+	char *input = NULL;
+	size_t bufsize = 0;
+
+	*i_eof = getline(&input, &bufsize, stdin);
+
+	return (input);
+}
