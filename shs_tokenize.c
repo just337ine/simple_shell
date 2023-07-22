8d253203341412794b7d5b1830647899d68b41e4
LadegbayeStella <ladegbayeewatomi50@gmail.com>
2023-07-21 17:04:17 -0700

    shs_execve.c

--- shs_tokenize.c
+++ shs_tokenize.c
@@ -10,29 +10,29 @@
  */
 void add_nodes(div_list **head_s, line_list **head_l, char *input)
 {
-    int i;
-    char *line;
+	int i;
+	char *line;
 
-    input = swap_char(input, 0);
+	input = swap_char(input, 0);
 
-    for (i = 0; input[i]; i++)
-    {
-        if (input[i] == ';')
-            add_div_node_end(head_s, input[i]);
+	for (i = 0; input[i]; i++)
+	{
+		if (input[i] == ';')
+			add_div_node_end(head_s, input[i]);
 
-        if (input[i] == '|' || input[i] == '&')
-        {
-            add_div_node_end(head_s, input[i]);
-            i++;
-        }
-    }
+		if (input[i] == '|' || input[i] == '&')
+		{
+			add_div_node_end(head_s, input[i]);
+			i++;
+		}
+	}
 
-    line = _strtok(input, ";|&");
-    do {
-        line = swap_char(line, 1);
-        add_line_node_end(head_l, line);
-        line = _strtok(NULL, ";|&");
-    } while (line != NULL);
+	line = _strtok(input, ";|&");
+	do {
+		line = swap_char(line, 1);
+		add_line_node_end(head_l, line);
+		line = _strtok(NULL, ";|&");
+	} while (line != NULL);
 }
 
 /**
@@ -45,36 +45,36 @@ void add_nodes(div_list **head_s, line_list **head_l, char *input)
  */
 void next_cmd(div_list **list_s, line_list **list_l, shell_data *shdata)
 {
-    int loop_sep;
-    div_list *ls_s;
-    line_list *ls_l;
+	int loop_sep;
+	div_list *ls_s;
+	line_list *ls_l;
 
-    loop_sep = 1;
-    ls_s = *list_s;
-    ls_l = *list_l;
+	loop_sep = 1;
+	ls_s = *list_s;
+	ls_l = *list_l;
 
-    while (ls_s != NULL && loop_sep)
-    {
-        if (shdata->status == 0)
-        {
-            if (ls_s->separator == '&' || ls_s->separator == ';')
-                loop_sep = 0;
-            if (ls_s->separator == '|')
-                ls_l = ls_l->next, ls_s = ls_s->next;
-        }
-        else
-        {
-            if (ls_s->separator == '|' || ls_s->separator == ';')
-                loop_sep = 0;
-            if (ls_s->separator == '&')
-                ls_l = ls_l->next, ls_s = ls_s->next;
-        }
-        if (ls_s != NULL && !loop_sep)
-            ls_s = ls_s->next;
-    }
+	while (ls_s != NULL && loop_sep)
+	{
+		if (shdata->status == 0)
+		{
+			if (ls_s->separator == '&' || ls_s->separator == ';')
+				loop_sep = 0;
+			if (ls_s->separator == '|')
+				ls_l = ls_l->next, ls_s = ls_s->next;
+		}
+		else
+		{
+			if (ls_s->separator == '|' || ls_s->separator == ';')
+				loop_sep = 0;
+			if (ls_s->separator == '&')
+				ls_l = ls_l->next, ls_s = ls_s->next;
+		}
+		if (ls_s != NULL && !loop_sep)
+			ls_s = ls_s->next;
+	}
 
-    *list_s = ls_s;
-    *list_l = ls_l;
+	*list_s = ls_s;
+	*list_l = ls_l;
 }
 
 /**
@@ -87,40 +87,40 @@ void next_cmd(div_list **list_s, line_list **list_l, shell_data *shdata)
  */
 int div_commands(shell_data *shdata, char *input)
 {
-    div_list *head_s, *list_s;
-    line_list *head_l, *list_l;
-    int loop;
+	div_list *head_s, *list_s;
+	line_list *head_l, *list_l;
+	int loop;
 
-    head_s = NULL;
-    head_l = NULL;
+	head_s = NULL;
+	head_l = NULL;
 
-    add_nodes(&head_s, &head_l, input);
+	add_nodes(&head_s, &head_l, input);
 
-    list_s = head_s;
-    list_l = head_l;
+	list_s = head_s;
+	list_l = head_l;
 
-    while (list_l)
-    {
-        shdata->input = list_l->line;
-        shdata->args = tokenize_line(shdata->input);
-        loop = exec_line(shdata);
-        free(shdata->args);
+	while (list_l)
+	{
+		shdata->input = list_l->line;
+		shdata->args = tokenize_line(shdata->input);
+		loop = exec_line(shdata);
+		free(shdata->args);
 
-        if (loop == 0)
-            break;
+		if (loop == 0)
+			break;
 
-        next_cmd(&list_s, &list_l, shdata);
+		next_cmd(&list_s, &list_l, shdata);
 
-        if (list_l)
-            list_l = list_l->next;
-    }
+		if (list_l)
+			list_l = list_l->next;
+	}
 
-    free_div_list(&head_s);
-    free_line_list(&head_l);
+	free_div_list(&head_s);
+	free_line_list(&head_l);
 
-    if (loop == 0)
-        return (0);
-    return (1);
+	if (loop == 0)
+		return (0);
+	return (1);
 }
 
 /**
@@ -131,39 +131,39 @@ int div_commands(shell_data *shdata, char *input)
  */
 char **tokenize_line(char *input)
 {
-    size_t bsize;
-    size_t i;
-    char **tokens;
-    char *token;
+	size_t bsize;
+	size_t i;
+	char **tokens;
+	char *token;
 
-    bsize = TOKEN_SIZE;
-    tokens = malloc(sizeof(char *) * (bsize));
-    if (!tokens)
-    {
-        write(STDERR_FILENO, ": allocation error\n", 18);
-        exit(EXIT_FAILURE);
-    }
+	bsize = TOKEN_SIZE;
+	tokens = malloc(sizeof(char *) * (bsize));
+	if (!tokens)
+	{
+		write(STDERR_FILENO, ": allocation error\n", 18);
+		exit(EXIT_FAILURE);
+	}
 
-    token = _strtok(input, TOKEN_DELIM);
-    tokens[0] = token;
+	token = _strtok(input, TOKEN_DELIM);
+	tokens[0] = token;
 
-    for (i = 1; token != NULL; i++)
-    {
-        if (i == bsize)
-        {
-            bsize += TOKEN_SIZE;
-            tokens = _realloc_ptr(tokens, i, sizeof(char *) * bsize);
-            if (!tokens)
-            {
-                write(STDERR_FILENO, ": allocation error\n", 18);
-                exit(EXIT_FAILURE);
-            }
-        }
-        token = _strtok(NULL, TOKEN_DELIM);
-        tokens[i] = token;
-    }
+	for (i = 1; token != NULL; i++)
+	{
+		if (i == bsize)
+		{
+			bsize += TOKEN_SIZE;
+			tokens = _realloc_ptr(tokens, i, sizeof(char *) * bsize);
+			if (!tokens)
+			{
+				write(STDERR_FILENO, ": allocation error\n", 18);
+				exit(EXIT_FAILURE);
+			}
+		}
+		token = _strtok(NULL, TOKEN_DELIM);
+		tokens[i] = token;
+	}
 
-    return (tokens);
+	return (tokens);
 }
 
 /**
@@ -175,36 +175,36 @@ char **tokenize_line(char *input)
  */
 char *swap_char(char *input, int bool)
 {
-    int i;
+	int i;
 
-    if (bool == 0)
-    {
-        for (i = 0; input[i]; i++)
-        {
-            if (input[i] == '|')
-            {
-                if (input[i + 1] != '|')
-                    input[i] = 16;
-                else
-                    i++;
-            }
+	if (bool == 0)
+	{
+		for (i = 0; input[i]; i++)
+		{
+			if (input[i] == '|')
+			{
+				if (input[i + 1] != '|')
+					input[i] = 16;
+				else
+					i++;
+			}
 
-            if (input[i] == '&')
-            {
-                if (input[i + 1] != '&')
-                    input[i] = 12;
-                else
-                    i++;
-            }
-        }
-    }
-    else
-    {
-        for (i = 0; input[i]; i++)
-        {
-            input[i] = (input[i] == 16 ? '|' : input[i]);
-            input[i] = (input[i] == 12 ? '&' : input[i]);
-        }
-    }
-    return (input);
+			if (input[i] == '&')
+			{
+				if (input[i + 1] != '&')
+					input[i] = 12;
+				else
+					i++;
+			}
+		}
+	}
+	else
+	{
+		for (i = 0; input[i]; i++)
+		{
+			input[i] = (input[i] == 16 ? '|' : input[i]);
+			input[i] = (input[i] == 12 ? '&' : input[i]);
+		}
+	}
+	return (input);
 }
